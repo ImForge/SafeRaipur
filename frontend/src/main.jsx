f4d87b1119b36324173b7ac8e85b97f241bed3ae
Shivam Tiwari <cdrshivam@gmail.com>
2026-07-08 20:39:22 +0530

    mainjsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 
+// Perf: the heatmap library reads canvas pixels repeatedly. Telling the
+// browser "we'll read this canvas often" lets it keep the buffer in a
+// faster-to-read place. This silences the Canvas2D willReadFrequently
+// warning and gives a small speed-up on weaker devices (phones).
+{
+  const orig = HTMLCanvasElement.prototype.getContext;
+  HTMLCanvasElement.prototype.getContext = function (type, attrs) {
+    if (type === '2d') attrs = { willReadFrequently: true, ...(attrs || {}) };
+    return orig.call(this, type, attrs);
+  };
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 );
